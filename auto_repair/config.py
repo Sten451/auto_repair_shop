f9563ee20d5d15896eaa14473e23f059a12f0353
Vyacheslav <lvv_1@list.ru>
2022-06-17 16:26:00 +0300

    main_template_done

---
+++ auto_repair/config.py
@@ -0,0 +1,6 @@
+
+class Config:
+    """Класс параметров конфигурации"""
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///flask_site.db'
+    SECRET_KEY = '3d6f45a5fc12445dbac2f59c3b6c7cb1'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
